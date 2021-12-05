a86fee81d16ddfbd9b1d37066e4570077ef92f0c
trizin <25263018+trizin@users.noreply.github.com>
2021-12-05 17:07:02 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const ethers = require("ethers");
+const cors = require("cors");
+const abi = require("./ERC20_ABI.json");
+
+const address = "0x6aa217312960a21adbde1478dc8cbcf828110a67";
+const bscProvider = new ethers.providers.JsonRpcProvider(
+  "https://bsc-dataseed.binance.org/"
+);
+const contract = new ethers.Contract(address, abi, bscProvider);
+
+app.use(cors());
+
+app.get("/totalSupply", async (req, res) => {
+  let totalSupply = await contract.totalSupply();
+  res.send(ethers.utils.formatEther(totalSupply));
+});
+
+app.get("/circSupply", async (req, res) => {
+  let totalSupply = await contract.CirculatingSupply();
+  res.send(ethers.utils.formatEther(totalSupply));
+});
+app.listen(4444, () => {
+  console.log("listening on port 3000");
+});
