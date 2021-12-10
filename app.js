c77c7928b8ac876d2717000539a2522e3b2a65c7
trizin <25263018+trizin@users.noreply.github.com>
2021-12-10 18:40:48 +0300

    Fix circ supply

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const abi = require("./ERC20_ABI.json");
 
 const address = "0x6aa217312960a21adbde1478dc8cbcf828110a67";
 const bscProvider = new ethers.providers.JsonRpcProvider(
-  "https://delicate-holy-sea.bsc.quiknode.pro/805724f094ab3267a2c7f380c5948052738576b9/"
+  "https://bsc-dataseed.binance.org/"
 );
 const contract = new ethers.Contract(address, abi, bscProvider);
 
@@ -18,7 +18,7 @@ app.get("/totalSupply", async (req, res) => {
 });
 
 app.get("/circSupply", async (req, res) => {
-  let totalSupply = await contract.balanceOf("0x89c68051543Fa135B31c2CE7BD8Cdf392345FF01");
+  let totalSupply = await contract.CirculatingSupply();
   res.send(ethers.utils.formatEther(totalSupply));
 });
 app.listen(80, () => {
